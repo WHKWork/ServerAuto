9231562eb06f64172a4c54887518703e9592d578
William Haagensen Knudsen <whkwork@hotmail.com>
2026-06-01 10:33:33 +0200

    Første commit

---
+++ CodeAlong/CodeAlongFunctions.py
@@ -0,0 +1,13 @@
+def myPrint(myString, myString2):
+    """
+    Prints user input plus to console
+
+    Parameters
+        str: myString
+        x: user Input
+
+    Returns
+        "Du skrev "user input"
+
+    """
+    print(myString, myString2)
